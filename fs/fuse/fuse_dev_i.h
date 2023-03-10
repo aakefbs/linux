70a1dad573ceafad64f99e61c4707d8cea870e7b
Bernd Schubert <bschubert@ddn.com>
2023-03-10 19:52:47 +0100

    fuse: Move fuse_get_dev to header file
    Another preparation patch, as this function will be needed by
    fuse/dev.c and fuse/dev_uring.c.
    
    Signed-off-by: Bernd Schubert <bschubert@ddn.com>
    cc: Miklos Szeredi <miklos@szeredi.hu>
    cc: linux-fsdevel@vger.kernel.org
    cc: Amir Goldstein <amir73il@gmail.com>
    cc: fuse-devel@lists.sourceforge.net

--- fs/fuse/fuse_dev_i.h
+++ fs/fuse/fuse_dev_i.h
@@ -7,6 +7,15 @@
 #ifndef _FS_FUSE_DEV_I_H
 #define _FS_FUSE_DEV_I_H
 
+static inline struct fuse_dev *fuse_get_dev(struct file *file)
+{
+	/*
+	 * Lockless access is OK, because file->private data is set
+	 * once during mount and is valid until the file is released.
+	 */
+	return READ_ONCE(file->private_data);
+}
+
 void fuse_dev_end_requests(struct list_head *head);
 
 #endif
