eb44c238b8ddf89403a793b16946cb83f3cd756f
Bernd Schubert <bschubert@ddn.com>
2024-08-31 01:26:26 +0200

    fuse: {uring} Handle IO_URING_F_TASK_DEAD
    The ring task is terminating, it not safe to still access
    its resources. Also no need for further actions.
    
    Signed-off-by: Bernd Schubert <bschubert@ddn.com>

--- fs/fuse/dev_uring.c
+++ fs/fuse/dev_uring.c
@@ -976,17 +976,22 @@ fuse_uring_send_req_in_task(struct io_uring_cmd *cmd,
 
 	BUILD_BUG_ON(sizeof(pdu) > sizeof(cmd->pdu));
 
+	if (unlikely(issue_flags & IO_URING_F_TASK_DEAD)) {
+		err = -ECANCELED;
+		goto terminating;
+	}
+
 	err = fuse_uring_prepare_send(ring_ent);
 	if (err)
 		goto err;
 
-	io_uring_cmd_done(cmd, 0, 0, issue_flags);
+terminating:
+	io_uring_cmd_done(cmd, err, 0, issue_flags);
 
 	spin_lock(&queue->lock);
 	ring_ent->state = FRRS_USERSPACE;
 	list_add(&ring_ent->list, &queue->ent_in_userspace);
 	spin_unlock(&queue->lock);
-
 	return;
 err:
 	fuse_uring_next_fuse_req(ring_ent, queue);
